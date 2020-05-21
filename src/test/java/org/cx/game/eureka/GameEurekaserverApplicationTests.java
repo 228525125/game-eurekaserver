12cccf9432806ab11dc116993c15596da265726a
chenxian <228525125@users.noreply.github.com>
2020-05-21 19:23:32 +0800

    Initial commit

---
+++ src/test/java/org/cx/game/eureka/GameEurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cx.game.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameEurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
